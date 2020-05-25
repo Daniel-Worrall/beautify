2dc5fbbd21b61865eaf2fec79d05dffa9dfb9b50
Daniel Worrall <Daniel.Worrall@hotmail.co.uk>
2020-05-25 10:46:32 +0100

    Encapsulate Runtimes (#4)
    * Add Circle
    
    * Encapsulate runtimes so they can be handled

--- src/beautify.cr
+++ src/beautify.cr
@@ -1,23 +1,41 @@
 require "duktape/runtime"
 
 module Beautify
-  {% for name in ["js", "css", "html"] %}
-    {{name.id.upcase}}_BEAUTIFY_JS   = \{{read_file("#{__DIR__}/../js-beautify/beautify-{{name.id}}.js")}}
+  @@runtime : Runtime? = nil
+  private FILES = [:js, :css, :html]
 
-    def self.init_{{name.id}}_runtime : Duktape::Runtime
-      runtime = Duktape::Runtime.new do |sandbox|
-        sandbox.eval!("global = {};") # Beautify assumes a global object to bind to.
-        sandbox.eval!({{name.id.upcase}}_BEAUTIFY_JS)
-      end
-      runtime
-    end
-
-    @@{{name.id}}_rt : Duktape::Runtime? = nil
+  {% for name in FILES %}
+    private {{name.id.upcase}}_BEAUTIFY_JS   = \{{read_file("#{__DIR__}/../js-beautify/beautify-{{name.id}}.js")}}
 
     def self.{{name.id}}(data : String) : String
-      @@{{name.id}}_rt ||= init_{{name.id}}_runtime
-      {{name.id}}_rt = @@{{name.id}}_rt.not_nil!
-      {{name.id}}_rt.call("global.{{name.id}}_beautify", data).as(String)
+      @@runtime ||= Runtime.new
+      runtime = @@runtime.not_nil!
+      runtime.{{name.id}}(data)
     end
   {% end %}
+
+  class Runtime
+    @[Flags]
+    private enum Initialised
+      {% for name in FILES %}
+        {{name.id.upcase}}
+      {% end %}
+    end
+
+    @flags = Initialised::None
+
+    @runtime = Duktape::Runtime.new do |sandbox|
+      sandbox.eval!("global = {};") # Beautify assumes a global object to bind to.
+    end
+
+    {% for name in FILES %}
+      def {{name.id}}(data : String) : String
+        unless @flags.{{name.id}}?
+          @runtime.exec({{name.id.upcase}}_BEAUTIFY_JS)
+          @flags |= Initialised::{{name.id.upcase}}
+        end
+        @runtime.call("global.{{name.id}}_beautify", data).as(String)
+      end
+    {% end %}
+  end
 end
